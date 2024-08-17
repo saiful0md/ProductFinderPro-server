518e0976b203dd4f62bd553a6dcb9fec42dc1aec
Saiful Islam <saiful islam>
2024-08-17 16:27:50 +0600

    added search query

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ app.use(cors({
 app.use(express.json())
 
 
-app.get('/', (req , res)=>{
-    res.send('ProductFinderPro')
+app.get('/', (req, res) => {
+  res.send('ProductFinderPro')
 })
 
-app.listen(port, ()=>{
-    console.log(`ProductFinderPro is running port: ${port}`);
+app.listen(port, () => {
+  console.log(`ProductFinderPro is running port: ${port}`);
 })
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xes5bsh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -36,17 +36,32 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    
+
     const allProductsCollection = client.db('productsFinderPro').collection('allProducts')
 
-app.get('/allProducts', async (req, res)=>{
-  const result = await allProductsCollection.find().toArray()
-  res.send(result)
-})
+    app.get('/allProducts', async (req, res) => {
+      const { page = 1, limit = 10, search = '', category, sort } = req.query;
+      console.log(search);
+      let query = {};
+      if (search) {
+        query.productName = { $regex: search, $options: 'i' };
+      }
+      let sortOrder = {};
+      if (sort === 'priceAsc') {
+        sortOrder.price = 1;
+      } else if (sort === 'priceDesc') {
+        sortOrder.price = -1;
+      } else if (sort === 'dateDesc') {
+        sortOrder.creationDate = -1;
+      }
+      const result = await allProductsCollection.find(query).sort(sortOrder).limit(parseInt(limit))
+      .skip((page - 1) * parseInt(limit)).toArray()
+      res.send(result)
+    })
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    
+
   }
 }
 run().catch(console.dir);
